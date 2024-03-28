3e1b36b23793467e392c20262ff02dd4427d6b36
Sárvári Ádám <sarvari2001@gmail.com>
2024-03-28 15:20:08 +0100

    c

--- SDLProject/src/Timer.cpp
+++ SDLProject/src/Timer.cpp
@@ -8,22 +8,23 @@ void Timer::getTick()
 {
 	Timer_DeltaTime = (SDL_GetTicks64() - Timer_LastTime);
 
-
+	constexpr auto egy_masodperc = 1000.0;
+	constexpr auto maxFPS = 500;
 	if (((Timer_DeltaTime < TARGET_DELTATIME) and fpslock) or (Timer_DeltaTime < TARGET_DELTATIME and menu)) {
 		SDL_Delay(static_cast<Uint32>(TARGET_DELTATIME - Timer_DeltaTime));
 		Timer_DeltaTime = static_cast<Uint64>(TARGET_DELTATIME);
 		countedframes++;
 	}
-	else if (Timer_DeltaTime < (1000 / 500)) {
-		SDL_Delay(static_cast<Uint32>((1000 / 500) - Timer_DeltaTime));
-		Timer_DeltaTime = 1000 / 500;
+	else if (Timer_DeltaTime < (egy_masodperc / maxFPS)) {
+		SDL_Delay(static_cast<Uint32>((egy_masodperc / maxFPS) - Timer_DeltaTime));
+		Timer_DeltaTime = egy_masodperc / maxFPS;
 		countedframes++;
 	}
 	else { countedframes++; }
 
 	sec += Timer_DeltaTime;
-	fps = static_cast<int>(countedframes / (sec / 1000.0));
-	sec = sec > 1000 ? 0 : sec;
+	fps = static_cast<int>(countedframes / (sec / egy_masodperc));
+	sec = sec > egy_masodperc ? 0 : sec;
 	countedframes = sec == 0 ? 0 : countedframes;
 
 	pressedtimer = !pressed ? 0 : pressedtimer -= static_cast<int>(Timer_DeltaTime);
